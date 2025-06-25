e74b64bedcce0e62213518a44bda63e69c403d21
rayhnn24 <raihan.kdg23@gmail.com>
2025-06-25 15:40:41 +0800

    membuat node baru

--- linkedlistantrian.cpp
+++ linkedlistantrian.cpp
@@ -4,6 +4,19 @@ using namespace std;
 struct Pasien {
     string nama;
     Pasien* next;
-    Pasien* head = nullptr;
 };
+
+Pasien* head = nullptr;
 void tambahPasien(string nama) {
+    Pasien* baru = new Pasien{nama, nullptr};
+
+    if (head == nullptr) {
+        head = baru;
+    } else {
+        Pasien* temp = head;
+        while (temp->next != nullptr)
+            temp = temp->next;
+        temp->next = baru;
+    }
+    cout << "Pasien \"" << nama << "\" ditambahkan ke antrian.\n";
+}
